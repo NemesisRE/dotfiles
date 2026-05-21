bbdf8c7c934dcc22e663334bc6b99be2c0826a35
Steven 'NemesisRE' Kurz <nemesissre@gmail.com>
2026-05-21 08:55:16 +0200

    feat(yazi): add function to install Yazi plugins and update renovate configuration

---
+++ dot_local/share/nredf/shell/common/functions/nredf_ya_pkg_install.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# vim: ts=2 sw=2 et ff=unix ft=bash syntax=sh
+
+function _nredf_ya_pkg_install() {
+  if _nredf_last_run; then
+    return 0
+  elif ! _nredf_create_lock; then
+    return 0
+  fi
+
+  if ! command -v ya &>/dev/null || [[ ! -f "${XDG_CONFIG_HOME}/yazi/package.toml" ]]; then
+    _nredf_remove_lock
+    return 0
+  fi
+
+  echo -e '\033[1mInstalling yazi plugins\033[0m'
+  ya pkg install >/dev/null 2>&1 || true
+  _nredf_last_run "" "true" "$(($(date +%s) + NREDF_24H_INTERVAL))"
+  _nredf_remove_lock
+}
